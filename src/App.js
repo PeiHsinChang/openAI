b8dad4360df1c14041f79eca79be7884ee2e4994
peihsin <zottycute@gmail.com>
2023-07-23 16:34:53 +0800

    add: .env to gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import dotenv from "dotenv";
+import React from "react";
+import { useState } from "react";
 
 dotenv.config();
 
 export function App() {
-    console.log(process.env)
-    return <h1>Hello world!!!!</h1>;
-  }
+  console.log(process.env.OPENAI_API_KEY);
+  const [inputValue, setInputValue] = useState("");
+  //   const []
+  const submitText = () => {
+    console.log("submitText");
+  };
 
-
+  return (
+    <>
+      <h1>Hello world!!!!</h1>
+      <input
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+          //   console.log(e.target.value);
+        }}
+      />
+      <button type="button" onClick={submitText}>
+        確認
+      </button>
+    </>
+  );
+}
